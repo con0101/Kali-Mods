1fb396956cf3ba81052954864fa976ef418372b7
con0101 <124743273+con0101@users.noreply.github.com>
2026-03-19 20:30:25 -0400

    Update kali-prep-2025.4.sh

--- kali-prep-2025.4.sh
+++ kali-prep-2025.4.sh
@@ -22,24 +22,70 @@ apt update
 #apt upgrade -y
 
 echo "--> Installing General Packages..."
-echo "------> gdb: "
+echo "------> gdb:"
 apt install -y gdb gdb-doc gdbserver libc-dbg
-echo "------> ghidra: "
+echo "------> ghidra:"
 apt install -y ghidra
-echo "------> CyberChef: "
+echo "------> CyberChef:"
 apt install -y cyberchef
-echo "------> pwntools: "
+echo "------> pwntools:"
 apt install -y python3-pwntools
-echo "------> pyftpdlib: "
+echo "------> pyftpdlib:"
 apt install -y python3-pyftpdlib
-echo "------> jq: "
+echo "------> jq:"
 apt install -y jq
-echo "------> ncat: "
+echo "------> tmux:"
+apt install -y tmux
+echo "------> ncat:"
 apt install -y ncat
-echo "------> rlwrap: "
+echo "------> rlwrap:"
 apt install -y rlwrap
-echo "------> seclists: "
+echo "------> seclists:"
 apt install -y seclists
+echo "------> peass-ng:"
+apt install -y peass
+echo "------> netexec:"
+apt install -y netexec
+echo "------> exiftool:"
+apt install -y libimage-exiftool-perl
+echo "------> pv:"
+apt install -y pv
+echo "------> xxd:"
+apt install -y xxd
+echo "------> gobuster:"
+apt install -y gobuster
+echo "------> ffuf:"
+apt install -y ffuf
+echo "------> wfuzz:"
+apt install -y wfuzz
+echo "------> nmap:"
+apt install -y nmap
+echo "------> masscan:"
+apt install -y masscan
+echo "------> mimikatz:"
+apt install -y mimikatz
+echo "------> impacket:"
+apt install -y python3-impacket
+echo "------> john:"
+apt install -y john
+echo "------> hydra:"
+apt install -y hydra
+echo "------> xfreerdp:"
+apt install -y freerdp3-x11
+echo "------> evil-winrm:"
+apt install -y evil-winrm
+echo "------> smbclient:"
+apt install -y smbclient
+echo "------> dnsrecon:"
+apt install -y dnsrecon
+echo "------> wpscan:"
+apt install -y wpscan
+echo "------> enum4linux:"
+apt install -y enum4linux
+echo "------> burpsuite:"
+apt install -y burpsuite
+
+
 
 echo "--> Local Copies of Common Tools/Utilities..."
 
@@ -62,4 +108,7 @@ echo "------> Modify ll alias to show hidden files in zsh and bash..."
 sudo -u kali sed -i "s/^alias ll=.*/alias ll='ls -lah'/" /home/kali/.zshrc
 sudo -u kali sed -i "s/^alias ll=.*/alias ll='ls -lah'/" /home/kali/.bashrc
 
+echo "------> GEF (GDB Enhanced Features)..."
+sudo -u kali wget -O ~/.gdbinit-gef.py -q https://gef.blah.cat/py
+sudo -u kali echo source ~/.gdbinit-gef.py >> ~/.gdbinit
 echo "Script Complete"
